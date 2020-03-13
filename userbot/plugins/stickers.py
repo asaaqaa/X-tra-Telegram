02ca4bff07dfc7483bd6384e87fa7a6a94050eeb
aaruu22 <59931090+aaruu22@users.noreply.github.com>
2020-03-14 02:46:01 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -47,7 +47,7 @@ async def _(event):
     me = borg.me
     userid = event.from_id
     packname = f"@CrossFireIsLit Ke Adde Mei {userid}"
-    packshortname = f"CrossFire_ke_adde_mei✔️_{userid}"  # format: Uni_Borg_userid
+    packshortname = f"CrossFire_ke_adde_mei_{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "@UniBorg_Sticker.png"
